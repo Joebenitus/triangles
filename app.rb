f302bea4c9e29a4d0bf8ede98229694efccff24f
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-28 15:53:11 -0700

    add GET/POST requests for determining triangle type
    Co-Authored by: David McCaslin <davidmccaslin94@gmail.com>

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/triangles')
+require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:layout)
+end
+
+get('/input') do
+  erb(:input)
+end
+
+post('/result') do
+  side1 = params[:side1]
+  side2 = params[:side2]
+  side3 = params[:side3]
+  triangle = Triangles.new(side1, side2, side3)
+  @type = triangle.type
+end
